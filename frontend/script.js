56d0765590f32d80dcd8f22fb880b9ae24251b11
Todd <toddeversole1@gmail.com>
2024-05-18 11:10:04 -0700

    added script.js

---
+++ frontend/script.js
@@ -0,0 +1,11 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Get a reference to the "Invest Now" button
+    var investButton = document.getElementById("invest-button");
+
+    // Add a click event listener to the button
+    investButton.addEventListener("click", function() {
+        // Replace this with your code to open the payment gateway pop-up/modal
+        alert("Payment gateway pop-up/modal will open here!");
+    });
+});
